64db1383ac7983af93fcbba509ff20729ee76b83
Nufayl Razick <nufayl.razick@ricardo.com>
2021-11-09 22:57:07 +0000

    feat: add App main compoenent and layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Main from "./layouts/Main";
+import Header from "./layouts/Header";
+import useBreweries from "./useBreweries";
+import BreweriesTable from "./BreweriesTable";
+import Grid from "@mui/material/Grid";
+import ListHeader from "./components/ListHeader";
+import FolderIcon from "@mui/icons-material/Folder";
+import ErrorAlert from "./components/ErrorAlert";
+import SelectField from "./components/SelectField";
+import LoadingSpinners from "./components/LoadingSpinners";
+import { ERROR_MESSAGE } from "./settings";
 
 function App() {
+  const {
+    loading,
+    breweryCollection,
+    breweryTypes,
+    error,
+    setActiveBreweryFilter,
+    activeBreweryFilter,
+  } = useBreweries();
+
+  if (error) return <ErrorAlert message={ERROR_MESSAGE} />;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Main>
+      <Grid container spacing={2}>
+        <Header />
+        <Grid item md={2} xs={12}>
+          <ListHeader title="Filter Breweries" Icon={<FolderIcon />} />
+          {breweryTypes && (
+            <SelectField
+              id="filter_brewery_type"
+              label="Filter by type"
+              defaultValue={
+                activeBreweryFilter ? activeBreweryFilter["by_type"] : "all"
+              }
+              options={breweryTypes}
+              handleChange={(event) => {
+                setActiveBreweryFilter({ by_type: event.target.value });
+              }}
+            />
+          )}
+        </Grid>
+        <Grid item md={10} xs={12}>
+          {loading ? (
+            <LoadingSpinners />
+          ) : (
+            <BreweriesTable tableData={breweryCollection} />
+          )}
+        </Grid>
+      </Grid>
+    </Main>
   );
 }
 
