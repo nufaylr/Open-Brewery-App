63ce236e2a801133b63f2ffd86ee939627a11a86
Nufayl Razick <nufayl.razick@ricardo.com>
2021-11-10 08:12:39 +0000

    refactor: BreweriesTable test

--- src/__test__/BreweriesTable.test.js
+++ src/__test__/BreweriesTable.test.js
@@ -4,7 +4,21 @@ import { MOCK_BREWERIES_RESPONSE } from "../API_MOCK";
 
 describe("BreweriesTable", () => {
   afterEach(cleanup);
-  test("Table", async () => {
+  test("Table headers", async () => {
     render(<BreweriesTable tableData={MOCK_BREWERIES_RESPONSE} />);
+    const rowsName = screen.getByText("Name", { closest: "row" });
+    const rowsType = screen.getByText("Type", { closest: "row" });
+    const rowsCountry = screen.getByText("Country", { closest: "row" });
+    const rowsStreet = screen.getByText("Street", { closest: "row" });
+    expect(rowsName).toBeInTheDocument();
+    expect(rowsType).toBeInTheDocument();
+    expect(rowsCountry).toBeInTheDocument();
+    expect(rowsStreet).toBeInTheDocument();
+  });
+
+  test("Table body", async () => {
+    render(<BreweriesTable tableData={MOCK_BREWERIES_RESPONSE} />);
+    const rowsName = screen.getAllByRole("row");
+    expect(rowsName[1]).toHaveTextContent(/dBeer Company/);
   });
 });
